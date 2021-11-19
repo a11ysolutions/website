cda797b9dde8a01b28432c2cc1d99670aa6eba04
Alberto Gonzalez Rosales <albexxl27@gmail.com>
2021-11-19 13:50:34 -0500

    feat: add first approach to Achievement Card component

--- src/Components/Functional/Functional.js
+++ src/Components/Functional/Functional.js
@@ -1,8 +1,32 @@
 import React from "react"
+import AchievementCard from "../UI/AchievementCard/AchievementCard"
 
 function Functional() {
     return (
-        <h1>Functional Page</h1>
+        <div>
+            <AchievementCard>
+                <p>
+                    <span style={{fontWeight: "bold"}}>Fully verified and reviewed
+                    results</span> within hours, with no
+                    extra triage required.
+                </p>
+            </AchievementCard>
+
+            <AchievementCard>
+                <p>
+                <span style={{fontWeight: "bold"}}>Shift your in-house testing
+                 workload to our 24/7/365</span> on-demand crowd to help you
+                  scale and execute tests in
+                   real-time.
+                </p>
+            </AchievementCard>
+
+            <AchievementCard>
+            <text>Fully verified and reviewed</text>
+            <text> results within hours, with no</text>
+            <text> extra triage required.</text>
+            </AchievementCard>
+        </div>
     )
 }
 
---
+++ src/Components/UI/AchievementCard/AchievementCard.js
@@ -0,0 +1,19 @@
+import React from "react"
+import './AchievementCard.css'
+
+function AchievementCard({ children, imgSrc }) {
+    return (
+        <div className="achievement-card">
+            <div style={{display: "flex", justifyContent: "center", alignItems: "center", columnGap: "18px"}}>
+                <div className='achievement-card-ellipse'>
+                    <img src={imgSrc}></img>
+                </div>
+                <div className='achievement-card-text-container'>
+                    {children}
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default AchievementCard;
