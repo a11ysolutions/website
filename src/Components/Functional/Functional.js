adce894a77e38ac0170e4feb9dfafeb8ae1cef6a
Alberto Gonzalez Rosales <albexxl27@gmail.com>
2021-11-20 16:48:18 -0500

    feat: add functional-header-text-button section

--- src/Components/Functional/Functional.js
+++ src/Components/Functional/Functional.js
@@ -2,6 +2,8 @@ import React from "react"
 import AchievementCard from "../UI/AchievementCard/AchievementCard"
 import FunctionalPresentationSign from "../UI/FunctionalPresentationSign/FunctionalPresentationSign"
 import FunctionalIlustration from "../UI/FunctionalIlustration/FunctionalIlustration"
+import Button from "../UI/Button/Button"
+import "./Functional.css"
 
 function Functional() {
     return (
@@ -11,6 +13,15 @@ function Functional() {
                 <div style={{ alignSelf: "center", marginTop: "90px" }}>
                     <FunctionalIlustration />
                 </div>
+            </div>
+            <div className="functional-header-text-button">
+                <h1><span style={{ color: "#0D9E71" }}>Our expert advice <br></br>can help you <br></br></span>solve your QA <br></br>bottlenecks</h1>
+                <p>
+                    Lorem ipsum dolor sit amet,consectetur adipiscing elit, sed do eiusmod tempor
+                    incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud
+                    exercitation ullamco laboris nisi ut aliquip ex ea.
+                    </p>
+                <Button>Get in touch</Button>
             </div>
             {/* <AchievementCard>
                 <p>
