ab4b789a48aabada9150ccb01a74fec5237e0b2e
Alberto Gonzalez Rosales <albexxl27@gmail.com>
2021-11-21 16:48:51 -0500

    fix: format files

--- src/Components/Functional/Functional.js
+++ src/Components/Functional/Functional.js
@@ -62,7 +62,7 @@ function Functional() {
                     Lorem ipsum dolor sit amet,consectetur adipiscing elit, sed do eiusmod tempor
                     incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud
                     exercitation ullamco laboris nisi ut aliquip ex ea.
-                    </p>
+                </p>
                 <Button size="large">Get a free consultation</Button>
             </div>
         </div>
