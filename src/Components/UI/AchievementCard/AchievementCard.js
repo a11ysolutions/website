7a73f13a3d06e94c7da3abc5dac59bc0b8a3c345
Alberto Gonzalez Rosales <albexxl27@gmail.com>
2021-11-22 09:12:29 -0500

    fix: format files with prettier

--- src/Components/UI/AchievementCard/AchievementCard.js
+++ src/Components/UI/AchievementCard/AchievementCard.js
@@ -1,19 +1,32 @@
-import React from "react"
-import './AchievementCard.css'
+import React from "react";
+import "./AchievementCard.css";
 
 function AchievementCard({ children, imgSrc }) {
-    return (
-        <div className="achievement-card">
-            <div style={{ display: "flex", justifyContent: "center", alignItems: "center", columnGap: "18px" }}>
-                <div className='achievement-card-ellipse' style={{justifyContent: "center", alignItems: "center"}}>
-                    <img src={imgSrc} className='achievement-card-img'></img>
-                </div>
-                <div className='achievement-card-text-container' style={{justifyContent: "center", alignItems: "center"}}>
-                    {children}
-                </div>
-            </div>
+  return (
+    <div className="achievement-card">
+      <div
+        style={{
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+          columnGap: "18px",
+        }}
+      >
+        <div
+          className="achievement-card-ellipse"
+          style={{ justifyContent: "center", alignItems: "center" }}
+        >
+          <img src={imgSrc} className="achievement-card-img"></img>
         </div>
-    )
+        <div
+          className="achievement-card-text-container"
+          style={{ justifyContent: "center", alignItems: "center" }}
+        >
+          {children}
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default AchievementCard;
