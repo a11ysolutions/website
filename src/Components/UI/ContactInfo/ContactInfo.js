1daaa4e7fcf0e223d61720b811d34668aaf49281
ernesto-code <60942733+ernesto-code@users.noreply.github.com>
2021-11-25 15:10:26 -0500

    new commit

--- src/Components/UI/ContactInfo/ContactInfo.js
+++ src/Components/UI/ContactInfo/ContactInfo.js
@@ -1,11 +1,14 @@
 import React from "react"
-//import "./Footer.css"
+import "./ContactInfo.css"
 //import Logo from "../Logo/Logo"
 
 function ContactInfo() {
     return (
-        <div>
-            ContactInfo
+        <div style={{ marginTop: "4em" }}>
+            <h2 style={{ textTransform: "uppercase", fontSize: "1em" }}>Contact us</h2>
+            <div className="contact-info-item">+1 (407) 535-1932</div>
+            <div className="contact-info-item">a11ycontact.info@gmail.com</div>
+            <div className="contact-info-item">Miami, Florida, US.</div>
         </div>
     )
 }
