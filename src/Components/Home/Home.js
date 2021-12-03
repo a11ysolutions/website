236363e68610a31f312bd4e3e23cf08183c39a1d
Amalia Ibarra <amalianibarra@gmail.com>
2021-12-03 11:04:21 -0500

    feat: add Archivo font family

--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -8,24 +8,31 @@ import Services from "../UI/Services/Services"
 import Toolset from "../UI/Toolset/Toolset"
 import Qualities from "../UI/Qualities/Qualities"
 import { scrollToContactForm } from "../Utils/Utils"
+import BicolorTitledText from "../UI/BicolorTitledText/BicolorTitledText"
 
 function Home() {
     return (
         <div style={{ display: "flex", flexDirection: "column", alignItems: "center" }}>
-            <div style={{ marginTop: "135px" }}>
+            <div className="a11y-home-presentation-container">
                 <PresentationSign />
                 <div style={{ alignSelf: "center", marginTop: "90px" }}>
                     <HomeIlustration />
                 </div>
             </div>
-            <div className="home-header-text-button" id="why-choose-us">
-                <h1>Why <span style={{ color: "#0D9E71" }}>choose us</span> ?</h1>
-                <p>
-                    Lorem ipsum dolor sit amet,consectetur adipiscing elit, sed do eiusmod tempor
-                    incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud
-                    exercitation ullamco laboris nisi ut aliquip ex ea.
+            <div>
+                <BicolorTitledText
+                    color1="black"
+                    color2="#0D9E71"
+                    color1Text="Why"
+                    color2Text="choose us?"
+                >
+                    <p>
+                        Lorem ipsum dolor sit amet,consectetur adipiscing elit, sed do eiusmod tempor
+                        incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud
+                        exercitation ullamco laboris nisi ut aliquip ex ea.
                     </p>
-                <Button onClick={scrollToContactForm}>Get in touch</Button>
+                    <Button onClick={scrollToContactForm}>Get in touch</Button>
+                </BicolorTitledText>
             </div>
             <Qualities />
             <Clients />
