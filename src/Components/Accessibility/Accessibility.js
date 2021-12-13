9504b0fd5540e00b95309302fa5907145fc60466
josejorgers <josejorgexl@gmail.com>
2021-12-13 21:43:16 +0100

    feat: add desktop view for a11y page

--- src/Components/Accessibility/Accessibility.js
+++ src/Components/Accessibility/Accessibility.js
@@ -88,31 +88,41 @@ function Accessibility() {
                     Creating your VPAT Document will help the world and potential customers know how accessible your products and services are to everyone.
                 </BicolorTitledText>
 
-                <Button onClick={scrollToContactForm} size="large" variant="negative">
-                    View a Sample VPAT
-                </Button>
-                <Button onClick={scrollToContactForm}
-                    className="automation"
-                >Get in touch</Button>
-
-                <div className="vpat vpat-explanation-header">WHAT IS A <b>VPAT</b>?</div>
-
-                <div className="vpat vpat-explanation-content">
-                    A VPAT is a clear documentation on how your digital product conforms to WCAG criteria. It clearly states which accessibility
-                    standards your application meets, and it advise users about any “accessibility blockers” they may face while using your product.
+                <div className="a11y-buttons">
+                    <Button onClick={scrollToContactForm} size="large" variant="negative">
+                        View a Sample VPAT
+                    </Button>
+                    <Button onClick={scrollToContactForm}
+                        className="automation"
+                    >Get in touch</Button>
                 </div>
                 
+            </section>
+
+            <section className="a11y-info-with-image inverted">
+                <div className="vpat a11y-page second-info">
+                    <div className="vpat vpat-explanation-header">WHAT IS A <b>VPAT</b>?</div>
+
+                    <div className="vpat vpat-explanation-content">
+                        A VPAT is a clear documentation on how your digital product conforms to WCAG criteria. It clearly states which accessibility
+                        standards your application meets, and it advise users about any “accessibility blockers” they may face while using your product.
+                    </div>
+
+                    <Button onClick={scrollToContactForm}
+                            className="automation"
+                        >Get in touch</Button>
+                </div>
+
                 <div className="intermediate-image">
                     <div className="automation-first-encircled-img">
                         <img src="./hand-with-laptop-circular.png" alt="A hand with an open laptop in the background" className="automation-first-image"/>
                     </div>
                     <div className="automation-first-blurred-circle"></div>
                 </div>
-                
             </section>
 
-            <section className="info-with-image">
-                <div className="a11y-page second-info">
+            <section className="a11y-info-with-image">
+                <div className="vpat a11y-page second-info">
                     <div className="vpat vpat-explanation-header">
                         WHY MY PRODUCT OR ORGANIZATION <b>NEEDS A VPAT</b>?
                     </div>
@@ -142,7 +152,7 @@ function Accessibility() {
                 
             </section>
             
-            <section className="info-with-image"> 
+            <section className="a11y-info-with-image inverted"> 
                 <div className="checked-info">
                     <BicolorTitledText
                         color1="black"
@@ -177,83 +187,93 @@ function Accessibility() {
                 </div>
             </section>
 
-            <section className="info-with-image shadowed-section"> 
+            <section className="a11y-info-with-image a11y-shadowed-section"> 
                 <div className="checked-info">
-                    <BicolorTitledText
-                        color1="black"
-                        color2="#0D9E71"
-                        color1Text="Our expert team"
-                        color2Text="will help you also with"
-                    ></BicolorTitledText>
+                    <div>
+                        <BicolorTitledText
+                            color1="black"
+                            color2="#0D9E71"
+                            color1Text="Our expert team"
+                            color2Text="will help you also with"
+                        ></BicolorTitledText>
 
-                    <div className="vpat-explanation-header no-margin">VERIFICATION TESTING</div>
+                        <div className="vpat-explanation-header no-margin">VERIFICATION TESTING</div>
 
-                    <div className="vpat-explanation-content no-margin">
-                        Once your team has fixed all the issues found in our initial audit accessibility assessment we will do the Verification Testing phase
-                        to confirm that your product finally met all the required accessibility guidelines.
-                        <br/><br/>
-                        Also, we will check that any change or addition that you did hasn’t introduce any other accessibility issues. This way we ensure
-                        your product is understood, navigated, used and accessible by everyone, regardless of the technology or platform used to access it.
+                        <div className="vpat-explanation-content no-margin">
+                            Once your team has fixed all the issues found in our initial audit accessibility assessment we will do the Verification Testing phase
+                            to confirm that your product finally met all the required accessibility guidelines.
+                            <br/><br/>
+                            Also, we will check that any change or addition that you did hasn’t introduce any other accessibility issues. This way we ensure
+                            your product is understood, navigated, used and accessible by everyone, regardless of the technology or platform used to access it.
+                        </div>
+                        <Button onClick={scrollToContactForm}
+                                className="automation"
+                            >Get in touch</Button>
+                    </div>
+
+                    <div className="last-image">
+                        <div className="automation-third-circle-container">
+                            <div className="automation-third-blurred-circle"></div>
+                            <div className="a11y-fourth-circle" ></div>
+                        </div>
+                        <img src="./approved-agreement-allowed-validation-concept.png" alt="A screen with the word approved" className="automation-fith-image" />
                     </div>
                 </div>
 
-                <div className="last-image">
-                    <div className="automation-third-circle-container">
-                        <div className="automation-third-blurred-circle"></div>
-                        <div className="automation-fourth-circle" ></div>
+
+                <div className="checked-info second">
+
+                    <div className="second-text-info">
+                        <div className="vpat-explanation-header no-margin">ACCESSIBILITY MONITOR</div>
+
+                        <div className="vpat-explanation-content no-margin">
+                            How do you ensure, every time you make any change in your code or add a new feature, your product remains accessible?
+                            <br/><br/>
+                            With Accessibility Monitoring we will test and verify periodically that your website or web application still met the
+                            Accessibility Standards and Guidelines required to keep your product accessible to every one.
+                        </div>
+                        <Button onClick={scrollToContactForm}
+                                className="automation"
+                            >Get in touch</Button>
                     </div>
-                    <img src="./approved-agreement-allowed-validation-concept.png" alt="A screen with the word approved" className="automation-fith-image" />
+
+                    <div className="last-image">
+                        <div className="automation-third-circle-container">
+                            <div className="automation-third-blurred-circle"></div>
+                            <div className="a11y-fourth-circle" ></div>
+                        </div>
+                        <img src="./result-economy-success-report-solution-statistics.png" alt="A person viewing charts on a paper" className="automation-fith-image" />
+                    </div>
                 </div>
 
-                <div className="checked-info">
-
-                    <div className="vpat-explanation-header no-margin">ACCESSIBILITY MONITOR</div>
-
-                    <div className="vpat-explanation-content no-margin">
-                        How do you ensure, every time you make any change in your code or add a new feature, your product remains accessible?
-                        <br/><br/>
-                        With Accessibility Monitoring we will test and verify periodically that your website or web application still met the
-                        Accessibility Standards and Guidelines required to keep your product accessible to every one.
-                    </div>
-                </div>
-
-                <Button onClick={scrollToContactForm}
-                        className="automation"
-                    >Get in touch</Button>
-
-                <div className="last-image">
-                    <div className="automation-third-circle-container">
-                        <div className="automation-third-blurred-circle"></div>
-                        <div className="automation-fourth-circle" ></div>
-                    </div>
-                    <img src="./result-economy-success-report-solution-statistics.png" alt="A person viewing charts on a paper" className="automation-fith-image" />
-                </div>
 
                 
                 <div className="checked-info">
 
-                    <div className="vpat-explanation-header no-margin">UX/UI AUDITS</div>
+                    <div>
+                        <div className="vpat-explanation-header no-margin">UX/UI AUDITS</div>
 
-                    <div className="vpat-explanation-content no-margin">
-                        <b>We help your UX/UI Team</b> when building new Products, Features, and Applications from the very beginning to ensure your design
-                        wireframes/mockups are in compliance with accessibility.
-                        <br/><br/>
-                        Our experts will provide detailed annotations to point out any potential accessibility issues and will help you define the best
-                        components so your Product will be WCAG compliant since Day 0.
+                        <div className="vpat-explanation-content no-margin">
+                            <b>We help your UX/UI Team</b> when building new Products, Features, and Applications from the very beginning to ensure your design
+                            wireframes/mockups are in compliance with accessibility.
+                            <br/><br/>
+                            Our experts will provide detailed annotations to point out any potential accessibility issues and will help you define the best
+                            components so your Product will be WCAG compliant since Day 0.
+                        </div>
+                        <Button onClick={scrollToContactForm}
+                                className="automation"
+                            >Get in touch</Button>
+
+                    </div>
+                    <div className="last-image">
+                        <div className="automation-third-circle-container">
+                            <div className="automation-third-blurred-circle"></div>
+                            <div className="a11y-fourth-circle" ></div>
+                        </div>
+                        <img src="./cooperation-analyst-chart-professional-paper-economics.png" alt="A person viewing charts on a paper" className="automation-fith-image" />
                     </div>
                 </div>
 
-                <Button onClick={scrollToContactForm}
-                        className="automation"
-                    >Get in touch</Button>
-
-                <div className="last-image">
-                    <div className="automation-third-circle-container">
-                        <div className="automation-third-blurred-circle"></div>
-                        <div className="automation-fourth-circle" ></div>
-                    </div>
-                    <img src="./cooperation-analyst-chart-professional-paper-economics.png" alt="A person viewing charts on a paper" className="automation-fith-image" />
-                </div>
             </section>
 
             <Toolset />
