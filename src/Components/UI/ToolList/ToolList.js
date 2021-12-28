85f1614cf1e0a3039c8e694db78220eccd08530b
Alberto Gonzalez Rosales <albexxl27@gmail.com>
2021-12-27 20:01:14 -0500

    feat: add first approach on ToolList component

---
+++ src/Components/UI/ToolList/ToolList.js
@@ -0,0 +1,56 @@
+import "./ToolList.css"
+
+function ToolList() {
+  return (
+    <div className="external-container">
+      <div className="row-container">
+        <img src="./aws-image.png" alt="amazon web services logo" className="tool-image"></img>
+        <img src="./jaws-image.png" alt="jaws logo" className="tool-image"></img>
+        <img src="./postman-image.png" alt="postman logo" className="tool-image"></img>
+      </div>
+
+      <div className="row-container">
+        <img src="./se-image.png" alt="selenium logo" className="tool-image"></img>
+        <img src="./awslambda-image.png" alt="amazon web services lambda logo" className="tool-image"></img>
+        <img src="./puppeteer-image.png" alt="puppeteer logo" className="tool-image"></img>
+      </div>
+
+      <div className="row-container">
+        <img src="./confluence-image.png" alt="confluence logo" className="tool-image"></img>
+        <img src="./browserstack-image.png" alt="browser stack logo" className="tool-image"></img>
+        <img src="./invision-image.png" alt="invision logo" className="tool-image"></img>
+      </div>
+
+      <div className="row-container">
+        <img src="./jenkins-image.png" alt="jenkins logo" className="tool-image"></img>
+        <img src="./voiceover-image.png" alt="voice over logo" className="tool-image"></img>
+        <img src="./nvda-image.png" alt="nvda logo" className="tool-image"></img>
+      </div>
+
+      <div className="row-container">
+        <img src="./talkback-image.png" alt="talk back logo" className="tool-image"></img>
+        <img src="./cucumber-image.png" alt="cucumber logo" className="tool-image"></img>
+        <img src="./figma-image.png" alt="figma logo" className="tool-image"></img>
+      </div>
+
+      <div className="row-container">
+        <img src="./jira-image.png" alt="jira logo" className="tool-image"></img>
+        <img src="./amp-image.png" alt="amp logo" className="tool-image"></img>
+        <img src="./ax-image.png" alt="ax logo" className="tool-image"></img>
+      </div>
+
+      <div className="row-container">
+        <img src="./xd-image.png" alt="xd logo" className="tool-image"></img>
+        <img src="./soapui-image.png" alt="soap ui logo" className="tool-image"></img>
+        <img src="./testng-image.png" alt="test ng logo" className="tool-image"></img>
+      </div>
+
+      <div className="row-container">
+        <img src="./abstract-image.png" alt="abstract logo" className="tool-image"></img>
+        <img src="./maven-image.png" alt="maven logo" className="tool-image"></img>
+      </div>
+    </div>
+  );
+}
+
+export default ToolList;
--- src/Components/UI/Toolset/Toolset.js
+++ src/Components/UI/Toolset/Toolset.js
@@ -1,7 +1,9 @@
 import "../../Home/Home.css";
 import Button from "../Button/Button";
-import { scrollToContactForm } from "../../Utils/Utils"
+import { scrollToContactForm } from "../../Utils/Utils";
 import BicolorTitledText from "../BicolorTitledText/BicolorTitledText";
+import ToolList from "../ToolList/ToolList";
+
 function Toolset() {
   return (
     <div style={{ marginTop: "60px" }}>
@@ -9,19 +11,19 @@ function Toolset() {
         color1="black"
         color2="#0D9E71"
         color1Text="Our"
-        color2Text="toolset">
+        color2Text="toolset"
+      >
         <p>
           Lorem ipsum dolor sit amet,consectetur adipiscing elit, sed do eiusmod
           tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
-          veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea.
-      </p>
-        <Button
-          size="large"
-          onClick={scrollToContactForm}
-        >
+          veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex
+          ea.
+        </p>
+        <Button size="large" onClick={scrollToContactForm}>
           Get a free consultation
         </Button>
       </BicolorTitledText>
+      <ToolList></ToolList>
     </div>
   );
 }
