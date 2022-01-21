78af946bb6ef95220159037dae74462e04b1d8b4
Jose Jorge Rodriguez Salgado <josejorgexl@gmail.com>
2022-01-21 10:24:19 +0100

    fix: change alt text of logo

--- src/Components/UI/Logo/Logo.js
+++ src/Components/UI/Logo/Logo.js
@@ -4,7 +4,7 @@ function Logo({ style, color, className }) {
     if (color === "green")
         return (<img src="/logo-green.png" alt="logo" style={{ ...style }} className={className} />)
 
-    return <img src="/logo.png" alt="logo" style={{ ...style }} className={className} />
+    return <img src="/logo.png" alt="A11y Solutions logo - link to home" style={{ ...style }} className={className} />
 }
 
 export default Logo
