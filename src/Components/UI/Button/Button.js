c8c54f8deb67cb8a90675a6520fe679c1c402dee
josejorgers <josejorgexl@gmail.com>
2022-01-25 14:53:39 +0100

    fix: change buttons to have link roles

--- src/Components/UI/Button/Button.js
+++ src/Components/UI/Button/Button.js
@@ -4,6 +4,7 @@ import "./Button.css"
 function Button({ children, size = "medium", variant = "dark", className = '', style, onClick }) {
     return (
         <button
+            role={'link'}
             className={`a11y-button a11y-button-${size} a11y-button-${variant} ${className}`}
             style={{ ...style }}
             onClick={onClick}
