bd8cb7dc4c8d0f6dcc01efea323f7eb3580e69e9
Alberto Gonzalez Rosales <albexxl27@gmail.com>
2022-01-25 16:54:09 -0500

    fix: remove logo text from alt text in Logo component

--- src/Components/UI/Logo/Logo.js
+++ src/Components/UI/Logo/Logo.js
@@ -2,9 +2,9 @@ import React from "react"
 
 function Logo({ style, color, className }) {
     if (color === "green")
-        return (<img src="/logo-green.png" alt="A11y Solutions logo" style={{ ...style }} className={className} />)
+        return (<img src="/logo-green.png" alt="A11y Solutions Footer" style={{ ...style }} className={className} />)
 
-    return <img src="/logo.png" alt="A11y Solutions logo" style={{ ...style }} className={className} />
+    return <img src="/logo.png" alt="A11y Solutions Navbar" style={{ ...style }} className={className} />
 }
 
 export default Logo
