b2572e73ee092fde4144a9828e609f441ebc1b5d
josejorgers <josejorgexl@gmail.com>
2022-01-27 13:49:23 +0100

    fix: remove hidden elements from DOM for navbar in desktop

--- src/Components/UI/SideDrawer/SideDrawer.js
+++ src/Components/UI/SideDrawer/SideDrawer.js
@@ -11,36 +11,36 @@ function SideDrawer({ show, onClose }) {
         attachedClasses = ["side-drawer", "open"]
 
     return (
-        <div className={attachedClasses.join(" ")} aria-hidden={!show}>
-            <div style={{ display: "flex", justifyContent: "space-between", margin: "21px" }}>
-                <Logo
-                    style={{
-                        marginLeft: "15px",
-                        height: "28px"
-                    }}
-                />
-                <button
-                    className="toggle-menu-btn"
-                    onClick={onClose}
-                >
-                    <IoCloseSharp color="black" size="25" />
-                </button>
-            </div>
-            <ul className="side-drawer-links">
-                <li><a onClick={onClose} href="/#why-choose-us">Why choose us</a></li>
-                <li><a onClick={onClose} href="/#our-services">Our services</a></li>
-                <li><a onClick={onClose} href="/#clients">Clients</a></li>
-                <li><a onClick={onClose} href="/#why-choose-us">Services Politics</a></li>
-                <li><a onClick={onClose} href="/#why-choose-us">Terms and Conditions</a></li>
-            </ul>
-            <div className="side-drawer-button">
-                    <Button
-                        onClick={() => { window.location.href = "#contact-us"; onClose() }}
-                    >
-                        Contact us
-                    </Button>
-            </div>
-        </div>
+        show && <div className={attachedClasses.join(" ")} aria-hidden={!show}>
+                    <div style={{ display: "flex", justifyContent: "space-between", margin: "21px" }}>
+                        <Logo
+                            style={{
+                                marginLeft: "15px",
+                                height: "28px"
+                            }}
+                        />
+                        <button
+                            className="toggle-menu-btn"
+                            onClick={onClose}
+                        >
+                            <IoCloseSharp color="black" size="25" />
+                        </button>
+                    </div>
+                    <ul className="side-drawer-links">
+                        <li><a onClick={onClose} href="/#why-choose-us">Why choose us</a></li>
+                        <li><a onClick={onClose} href="/#our-services">Our services</a></li>
+                        <li><a onClick={onClose} href="/#clients">Clients</a></li>
+                        <li><a onClick={onClose} href="/#why-choose-us">Services Politics</a></li>
+                        <li><a onClick={onClose} href="/#why-choose-us">Terms and Conditions</a></li>
+                    </ul>
+                    <div className="side-drawer-button">
+                            <Button
+                                onClick={() => { window.location.href = "#contact-us"; onClose() }}
+                            >
+                                Contact us
+                            </Button>
+                    </div>
+                </div>
     )
 }
 
--- src/Components/UI/ToggleMenu/ToggleMenu.js
+++ src/Components/UI/ToggleMenu/ToggleMenu.js
@@ -1,12 +1,21 @@
-import React from "react"
+import React, { useEffect } from "react"
 import { IoMenu } from "react-icons/io5"
 import "./ToggleMenu.css"
 
 function ToggleMenu({ onClick }) {
+
+    const [isHidden, setIsHidden] = React.useState(false)
+
+    useEffect(() => {
+        const btn = document.querySelector(".toggle-menu-btn")
+        const style = getComputedStyle(btn)
+
+        setIsHidden(style.display === "none")
+    }, [])
     return (
-        <button className="toggle-menu-btn" onClick={onClick}>
-            <IoMenu color="black" size="24" />
-        </button>
+        isHidden || <button className="toggle-menu-btn" onClick={onClick}>
+                        <IoMenu color="black" size="24" />
+                    </button>
     )
 }
 
