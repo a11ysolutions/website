fc3ab7ea5378f46dca946d4c86107edc3f14a11f
atamayo <atamayoquintero@gmail.com>
2023-08-31 14:37:15 -0500

    feat: add conditional content display

--- src/Components/UI/HomeIlustration/HomeIlustration.js
+++ src/Components/UI/HomeIlustration/HomeIlustration.js
@@ -1,41 +1,41 @@
-import React from "react"
-import Pill from "../Pill/Pill"
-import "./HomeIlustration.css"
+import React from 'react'
+import Pill from '../Pill/Pill'
+import './HomeIlustration.css'
 
-function HomeIlustration({ firstPillText, secondPillText, thirdPillText }) {
-    return (
-        <div className="home-ilustration-container">
-            <div style={{ position: "relative", marginTop: "50px" }}>
-                <img
-                    src="/coding-man@2x.png"
-                    className="home-ilustration"
-                    alt="We work passionately on functional testing, accessibility testing, and automation testing services."
-                    style={{ zIndex: "2" }}
-                />
+function HomeIlustration({ firstPillText, secondPillText, thirdPillText, src }) {
+  return (
+    <div className="home-ilustration-container">
+      <div style={{ position: 'relative', marginTop: '50px' }}>
+        <img
+          src={src ? src : '/coding-man@2x.png'}
+          className="home-ilustration"
+          alt="We work passionately on functional testing, accessibility testing, and automation testing services."
+          style={{ zIndex: '2' }}
+        />
 
-                {firstPillText &&
-                    <Pill className="home-ilustration-functionality-pill">
-                        {firstPillText}
-                    </Pill>}
-                {secondPillText &&
-                    <Pill
-                        className="home-ilustration-accessibility-pill"
-                        dotPosition="top-right">
-                        {secondPillText}
-                    </Pill>}
-                {thirdPillText &&
-                    <Pill
-                        className="home-ilustration-automation-pill"
-                        dotPosition="top-left">
-                        {thirdPillText}
-                    </Pill>}
-                <div className="home-ilustration-small-circle"></div>
-                <div style={{ width: "100%" }}>
-                    <div className="home-ilustration-large-circle"></div>
-                </div>
+        {firstPillText && <Pill className="home-ilustration-functionality-pill">{firstPillText}</Pill>}
+        {secondPillText && (
+          <Pill className="home-ilustration-accessibility-pill" dotPosition="top-right">
+            {secondPillText}
+          </Pill>
+        )}
+        {thirdPillText && (
+          <Pill className="home-ilustration-automation-pill" dotPosition="top-left">
+            {thirdPillText}
+          </Pill>
+        )}
+
+        {!src && (
+          <>
+            <div className="home-ilustration-small-circle"></div>
+            <div style={{ width: '100%' }}>
+              <div className="home-ilustration-large-circle"></div>
             </div>
-        </div>
-    )
+          </>
+        )}
+      </div>
+    </div>
+  )
 }
 
 export default HomeIlustration
