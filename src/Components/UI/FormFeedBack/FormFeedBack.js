53cd330d273fb89fec37a4a687ee441f3fc64cc3
atamayo <atamayoquintero@gmail.com>
2023-09-05 10:20:23 -0500

    refactor: removed form validation

--- src/Components/UI/FormFeedBack/FormFeedBack.js
+++ src/Components/UI/FormFeedBack/FormFeedBack.js
@@ -1,48 +1,25 @@
-import React, { useState, useEffect, useRef } from 'react'
+import React, { useState, useRef } from 'react'
 import './FormFeedBack.css'
 import Button from '../Button/Button'
 
 function FormFeedBack() {
   const initialValues = { email: '', message: '' }
-
   const COMPANY_EMAIL = 'felipe.perez@a11ysolutions.com'
-
   const [formValues, setFormValues] = useState(initialValues)
-  const [formErrors, setFormErrors] = useState({})
-  let isSubmit = false
-  //const [isSubmit, setIsSubmit] = useState(false)
-
   const emailRef = useRef(null)
-
   const SUBJECT = `Interested from `
-
-  let firstFieldWithError = null
-  let attribute = { 'aria-invalid': 'true' }
 
   const changeHandler = (event) => {
     const { id, value } = event.target
     setFormValues({ ...formValues, [id]: value })
-    setFormErrors({ ...formErrors, [id]: '' })
   }
-
-  useEffect(() => {
-    if (Object.keys(formErrors).length === 0 && isSubmit) {
-    }
-  }, [formErrors, isSubmit])
 
   const onSubmit = (event) => {
     event.preventDefault()
-    const validationErrors = validate(formValues)
-    setFormErrors(validationErrors)
-
-    if (Object.keys(validationErrors).length === 0) {
-      // Submit the form
-
       const BODY = `${formValues.message}
 ---------------------------
 
             `
-
       const URL_SUBJECT = encodeURIComponent(SUBJECT)
       const URL_BODY = encodeURIComponent(BODY)
 
@@ -50,21 +27,6 @@ function FormFeedBack() {
 
       // Reset values
       setFormValues(initialValues)
-    }
-  }
-  const validate = (values) => {
-    const errors = {}
-    const regex = /^[^\s@]+@[^\s@]+\.[^\s@]{2,}$/i
-    if (!values.email) {
-      errors.email = 'Please enter an email address'
-      if (!firstFieldWithError) firstFieldWithError = emailRef
-    } else if (!regex.test(values.email)) {
-      errors.email = 'Please enter a valid email'
-      if (!firstFieldWithError) firstFieldWithError = emailRef
-    }
-    if (firstFieldWithError) firstFieldWithError.current.focus()
-
-    return errors
   }
 
   return (
@@ -78,18 +40,13 @@ function FormFeedBack() {
               </label>
               <input
                 id="email"
-                className={formErrors['email'] ? 'form-fb-input error' : 'form-fb-input'}
+                className='form-fb-input'
                 ref={emailRef}
                 type="email"
                 autoComplete="email"
                 value={formValues.email}
                 onChange={changeHandler}
-                aria-describedby="email-error"
-                {...(formErrors['email'] && attribute)}
               />
-              <div id="email-error" className="error-message">
-                {formErrors.email}
-              </div>
             </div>
           </div>
           <div style={{ paddingTop: '12px' }}>
