f15a0e68de05cb38454e1631e3e6c39dc5dbe966
atamayo <atamayoquintero@gmail.com>
2023-09-04 20:00:17 -0500

    feat: add section feedback to AlliBot page

--- src/Components/A11yBot/A11yBot.js
+++ src/Components/A11yBot/A11yBot.js
@@ -5,28 +5,29 @@ import Presentation from '../UI/Presentation/Presentation'
 import BicolorTitledText from '../UI/BicolorTitledText/BicolorTitledText'
 import CustomCard from '../UI/CustomCard/CustomCard'
 import Button from '../UI/Button/Button'
+import FormFeedBack from '../UI/FormFeedBack/FormFeedBack'
 import './A11yBot.css'
 
 function A11yBot() {
   return (
-    <div className="a11yBot-page-container">
+    <div id="a11yBot" className="a11yBot-page-container">
       <Helmet>
-        <title>a11yBot - Empowering Inclusive Experiences | A11ysolutions</title>
+        <title>AlliBot - Empowering Inclusive Experiences | A11ysolutions</title>
       </Helmet>
 
       <div className="a11yBot-presentation-container">
         <div className="page-top">
           <Presentation
-            title="a11yBot - Empowering Inclusive Experiences"
+            title="AlliBot - Empowering Inclusive Experiences"
             texts={[
               'A chatbot designed to provide assistance and answer any accessibility questions based on the WCAG 2.1 Standard.',
             ]}
-            buttonText="Get a free consultation"
+            buttonText="TRY IT NOW"
             className="a11yBot-presentation-sign"
           />
         </div>
         <div className="privacy-policy-ilustration-container">
-          <HomeIlustration src="/a11yBot.png" />
+          <HomeIlustration src="/alliBot.png" />
         </div>
       </div>
 
@@ -62,7 +63,7 @@ function A11yBot() {
           <CustomCard imgSrc="/user-friendly-interface.jpg" className="shadow-none">
             <h3>User-Friendly Interface</h3>
             <p>
-              a11yBot features an intuitive interface, making it easy for users to interact and seek
+              AlliBot features an intuitive interface, making it easy for users to interact and seek
               assistance.
             </p>
           </CustomCard>
@@ -79,58 +80,82 @@ function A11yBot() {
           </div>
         </div>
 
-        <div className="a11yBot-review">
+        <div className="a11yBot-feedback ">
           <div className="home-services-bicolored-text">
             <BicolorTitledText color1="black" color2="#0D9E71" className="home-our-services">
-              <p>Some awesome reviews</p>
+              <p>We are committed to creating a world that is both inclusive and equitable for everyone.</p>
             </BicolorTitledText>
           </div>
 
-          <div className="a11yBot-review-container">
-            <CustomCard
-              imgSrc="/yainery-bolanos.png"
-              className="custom-horizontal-card"
-              classTextContainer="text-container-horizontal"
-              classEllipseBar="card-ellipse-bar"
-              icon
-            >
-              <h3>Yainery Bolanos, CEO/Co-Founder at Adaved</h3>
-              <p>
-                a11yBot has streamlined our workflow, helped our team to ensure compliance and even educated
-                them around web accessibility. I highly recommend it to any organization that is serious about
-                making the web a more inclusive space.
-              </p>
-            </CustomCard>
-            <CustomCard
-              imgSrc="/luis-terrero.png"
-              className="custom-horizontal-card shadow"
-              classTextContainer="text-container-horizontal"
-              classEllipseBar="card-ellipse-bar"
-              icon
-            >
-              <h3>Luis Terrero, CEO/Founder at PixelPerfecto</h3>
-              <p>
-                a11yBot has empowered us to be proactive rather than reactive when it comes to accessibility.
-                This is not just a problem-solving tool, it's an educational resource, the chatbot have helped
-                our team deepen their understanding of web accessibility.
-              </p>
-            </CustomCard>
-            <CustomCard
-              imgSrc="/dariel-vila.png"
-              className="custom-horizontal-card"
-              classTextContainer="text-container-horizontal"
-              classEllipseBar="card-ellipse-bar"
-              icon
-            >
-              <h3>Dariel Vila, CEO/Co-Founder at Front10 and Woxo</h3>
-              <p>
-                a11yBot has been a game-changer for us. One of the significant challenges we've faced is the
-                need for quick, accurate answers to complex accessibility questions and a11yBot has been
-                invaluable in this regard, providing us with instant and precise solutions to any web
-                accessibility query we throw at it, saving us countless hours of research and potential trial
-                and error.
-              </p>
-            </CustomCard>
+          <div className="a11yBot-feedback-content">
+            <div className="a11yBot-feedback-text">
+              <div>
+                <p>
+                  Your feedback on AlliBot is invaluable to us and instrumental in refining its capabilities.
+                  Please share your experience to help us enhance the service for you and others.
+                </p>
+                <p>Thank you for your thoughtful contribution!</p>
+              </div>
+            </div>
+            <div className="a11yBot-feedback-form" id="footer-form">
+              <FormFeedBack />
+            </div>
+          </div>
+        </div>
+
+        <div className="a11yBot-review-container">
+          <div className="a11yBot-review">
+            <div className="home-services-bicolored-text">
+              <BicolorTitledText color1="black" color2="#0D9E71" className="home-our-services">
+                <p>Some awesome reviews</p>
+              </BicolorTitledText>
+            </div>
+
+            <div className="a11yBot-review-container">
+              <CustomCard
+                imgSrc="/yainery-bolanos.png"
+                className="custom-horizontal-card"
+                classTextContainer="text-container-horizontal"
+                classEllipseBar="card-ellipse-bar"
+                icon
+              >
+                <h3>Yainery Bolanos, CEO/Co-Founder at Adaved</h3>
+                <p>
+                  AlliBot has streamlined our workflow, helped our team to ensure compliance and even educated
+                  them around web accessibility. I highly recommend it to any organization that is serious
+                  about making the web a more inclusive space.
+                </p>
+              </CustomCard>
+              <CustomCard
+                imgSrc="/luis-terrero.png"
+                className="custom-horizontal-card shadow"
+                classTextContainer="text-container-horizontal"
+                classEllipseBar="card-ellipse-bar"
+                icon
+              >
+                <h3>Luis Terrero, CEO/Founder at PixelPerfecto</h3>
+                <p>
+                  AlliBot has empowered us to be proactive rather than reactive when it comes to
+                  accessibility. This is not just a problem-solving tool, it's an educational resource, the
+                  chatbot have helped our team deepen their understanding of web accessibility.
+                </p>
+              </CustomCard>
+              <CustomCard
+                imgSrc="/dariel-vila.png"
+                className="custom-horizontal-card"
+                classTextContainer="text-container-horizontal"
+                classEllipseBar="card-ellipse-bar"
+                icon
+              >
+                <h3>Dariel Vila, CEO/Co-Founder at Front10 and Woxo</h3>
+                <p>
+                  Even though AlliBot is still in its initial phase, AlliBot has been a game-changer for us.
+                  In regards to accessibility, one of the significant challenges we've faced is the need for
+                  quick, accurate answers to complex accessibility questions and AlliBot has been invaluable
+                  in this regard, saving us hours of research and trial and error.
+                </p>
+              </CustomCard>
+            </div>
           </div>
         </div>
       </div>
