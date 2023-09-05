a7f2c0a814fe013ff41e0d86a577e82f902f673a
atamayo <atamayoquintero@gmail.com>
2023-09-04 20:02:16 -0500

    fix: update OurProducts Section

--- src/Components/UI/OurProducts/OurProducts.js
+++ src/Components/UI/OurProducts/OurProducts.js
@@ -20,20 +20,20 @@ function OurProducts() {
         <div className="our-products-container">
           <div className="our-products-item_photo">
             <img
-              src="/a11yBot.png"
+              src="/alliBot.png"
               className=""
               alt="No more endless Googling or diving into forums for answers to your web accessibility questions. a11yBot provides immediate, expert guidance, effectively becoming your on-demand accessibility consultant."
               style={{}}
             />
           </div>
           <div className="our-products-item_text">
-            <h2>a11yBot: Your AI-driven expert for instant and tailored accessibility solutions</h2>
+            <h2>AlliBot: Your AI-driven expert for instant and tailored accessibility solutions</h2>
             <p>
               No more endless Googling or diving into forums for answers to your web accessibility questions.
               a11yBot provides immediate, expert guidance, effectively becoming your on-demand accessibility
               consultant.
             </p>
-            <Button onClick={() => (document.location.href = '/a11yBot')} size="large">
+            <Button onClick={() => (document.location.href = '/alliBot')} size="large">
               Learn More
             </Button>
           </div>
@@ -44,19 +44,21 @@ function OurProducts() {
             <img
               src="/a11yWidget.png"
               className=""
-              alt="Provide real-time accessibility adjustments. The a11yWidget ensures that all visitors, regardless of their abilities, can interact with your site effortlessly. By integrating our Accessibility Widget, you're not just adding a feature to your website; you're elevating the entire user experience."
+              alt="Provide real-time accessibility adjustments. The AlliWidget ensures that all visitors, regardless of their abilities, can interact with your site effortlessly. By integrating our Accessibility Widget, you're not just adding a feature to your website; you're elevating the entire user experience."
               style={{}}
             />
           </div>
           <div className="our-products-item_text">
-            <h2>a11yWidget: Real-Time Solutions, Zero Delays</h2>
+            <h2>AlliWidget: Real-Time Solutions, Zero Delays</h2>
             <p>
-              Provide real-time accessibility adjustments. The a11yWidget ensures that all visitors,
+              Provide real-time accessibility adjustments. The AlliWidget ensures that all visitors,
               regardless of their abilities, can interact with your site effortlessly. By integrating our
               Accessibility Widget, you're not just adding a feature to your website; you're elevating the
               entire user experience.
             </p>
-            <Button size="large">Learn More</Button>
+            <Button onClick={() => (document.location.href = '/alliWidget')} size="large">
+              Learn More
+            </Button>
           </div>
         </div>
       </div>
