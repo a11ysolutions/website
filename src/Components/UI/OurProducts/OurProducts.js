a569675c50a160d2225dbef356b4218a9a89c3e7
atamayo <atamayoquintero@gmail.com>
2023-09-05 17:20:52 -0500

    fix: update ourProducts section header

--- src/Components/UI/OurProducts/OurProducts.js
+++ src/Components/UI/OurProducts/OurProducts.js
@@ -27,7 +27,7 @@ function OurProducts() {
             />
           </div>
           <div className="our-products-item_text">
-            <h2>AlliBot: Your AI-driven expert for instant and tailored accessibility solutions</h2>
+            <h3>AlliBot: Your AI-driven expert for instant and tailored accessibility solutions</h3>
             <p>
               No more endless Googling or diving into forums for answers to your web accessibility questions.
               a11yBot provides immediate, expert guidance, effectively becoming your on-demand accessibility
@@ -49,7 +49,7 @@ function OurProducts() {
             />
           </div>
           <div className="our-products-item_text">
-            <h2>AlliWidget: Real-Time Solutions, Zero Delays</h2>
+            <h3>AlliWidget: Real-Time Solutions, Zero Delays</h3>
             <p>
               Provide real-time accessibility adjustments. The AlliWidget ensures that all visitors,
               regardless of their abilities, can interact with your site effortlessly. By integrating our
