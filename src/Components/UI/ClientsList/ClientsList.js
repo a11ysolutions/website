deea7f63d493c034ebcc9f920f8958de932f7c08
Davy1988 <dsecoosorio@gmail.com>
2024-02-19 18:11:54 -0500

    ref: update the client component

---
+++ src/Components/UI/ClientsList/ClientsList.js
@@ -0,0 +1,17 @@
+import "./ClientsList.css";
+function ClientsList({ clients }) {
+  return (
+    <div className="container-clients">
+      <div class="content-grid-clients" role={"list"}>
+        {clients.map((c, index) => {
+          return (
+            <div key={index} className="clients-img-card" role={"listitem"}>
+              <img alt={c.alt} src={c.src} style={c.style} />
+            </div>
+          );
+        })}
+      </div>
+    </div>
+  );
+}
+export { ClientsList };
