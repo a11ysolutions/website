d71651b7af23e0584f890bca6f81d3aef20cca15
Davy1988 <dsecoosorio@gmail.com>
2024-03-15 17:20:37 -0400

    ref: add the landing page design to main page and Privacy Police page

--- src/Components/UI/ServiceCard/ServiceCard.js
+++ src/Components/UI/ServiceCard/ServiceCard.js
@@ -1,28 +1,45 @@
-import React from "react"
-import "./ServiceCard.css"
-import Button from "../Button/Button"
+import React from "react";
+import "./ServiceCard.css";
+import Button from "../Button/Button";
 
-function ServiceCard({ children, header, imgSrc, imgColor = "white", style, onClickButton, imgStyle, imgClass, ariaLabel }) {
-    return (
-        <div className="service-card-container" style={{ ...style }} role={'listitem'}>
-            <div className="service-card" style={{ backgroundColor: imgColor, ...imgStyle }}>
-                <img
-                    alt=""
-                    src={imgSrc}
-                    className={imgClass}
-                />
-            </div>
-            <div className="service-card-content">
-                <h3>{header}</h3>
-                <div className="service-card-text">
-                    {children}
-                </div>
-                <div style={{ marginTop: "27px" }}>
-                    <Button onClick={onClickButton} ariaLabel={ariaLabel}>Learn More</Button>
-                </div>
-            </div>
+function ServiceCard({
+  children,
+  header,
+  imgSrc,
+  imgColor = "white",
+  style,
+  onClickButton,
+  imgStyle,
+  imgClass,
+  ariaLabel,
+}) {
+  return (
+    <div
+      className="service-card-container"
+      style={{ ...style }}
+      role={"listitem"}
+    >
+      <div
+        className="service-card"
+        style={{ backgroundColor: imgColor, ...imgStyle }}
+      >
+        <img alt="" src={imgSrc} className={imgClass} />
+      </div>
+      <div className="service-card-content">
+        <h3>{header}</h3>
+        <div className="service-card-text">{children}</div>
+        <div style={{ marginTop: "27px" }}>
+          <Button
+            variant="negative"
+            onClick={onClickButton}
+            ariaLabel={ariaLabel}
+          >
+            Learn More
+          </Button>
         </div>
-    )
+      </div>
+    </div>
+  );
 }
 
-export default ServiceCard
+export default ServiceCard;
