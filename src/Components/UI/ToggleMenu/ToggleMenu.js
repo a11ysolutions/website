b93e96a5f7fe3d5a271c60bf558100bd5c3ec019
atamayo <atamayoquintero@gmail.com>
2024-03-18 10:07:15 -0500

    fix: improve hamburger menu accessibility

--- src/Components/UI/ToggleMenu/ToggleMenu.js
+++ src/Components/UI/ToggleMenu/ToggleMenu.js
@@ -1,22 +1,22 @@
-import React, { useEffect } from "react"
-import { IoMenu } from "react-icons/io5"
-import "./ToggleMenu.css"
+import React, { useEffect } from 'react'
+import { IoMenu } from 'react-icons/io5'
+import './ToggleMenu.css'
 
-function ToggleMenu({ onClick }) {
+function ToggleMenu({ onClick,menuButtonRef }) {
+  //   const [isHidden, setIsHidden] = React.useState(false)
 
-    const [isHidden, setIsHidden] = React.useState(false)
+  //   useEffect(() => {
+  //     const btn = document.querySelector('.toggle-menu-btn')
+  //     const style = getComputedStyle(btn)
 
-    useEffect(() => {
-        const btn = document.querySelector(".toggle-menu-btn")
-        const style = getComputedStyle(btn)
-
-        setIsHidden(style.display === "none")
-    }, [])
-    return (
-        isHidden || <button className="toggle-menu-btn" onClick={onClick}>
-                        <IoMenu color="black" size="24" />
-                    </button>
-    )
+  //     setIsHidden(style.display === 'none')
+  //   }, [])
+  return (
+    // isHidden || (
+    <button className="toggle-menu-btn" aria-label="Open Menu" onClick={onClick} ref={menuButtonRef}>
+      <IoMenu color="black" size="24" aria-hidden="true" />
+    </button>
+  )
 }
 
 export default ToggleMenu
